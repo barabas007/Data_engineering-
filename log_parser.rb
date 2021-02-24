1646d94ee5fad13a7daf2013333d2b22aa1f3c4c
barabas007 <shop.ru.shop@mail.ru>
2021-02-24 11:00:11 +0300

    added signup_log_lines

--- log_parser.rb
+++ log_parser.rb
@@ -1,6 +1,10 @@
 filename = 'access_log_2021.log'
 
-access_log = File.open(filename, 'r') do |f|
-  f.readline
+access_log_lines = File.open(filename, 'r') do |f|
+  f.readlines
 end
-puts access_log.length 
+signup_log_lines = access_log_lines.select do |line|
+  line.include?('/signup?email=')
+end
+
+puts signup_log_lines
